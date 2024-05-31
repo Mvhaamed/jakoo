76f468f05045ab78374fb1cb19f551df223dbe99
Mvhaamed <jakooyad012@gmail.com>
2024-05-31 16:10:31 +0300

    Add files via upload

---
+++ VeGaMusic/core/mongo.py
@@ -0,0 +1,16 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from config import MONGO_DB_URI
+
+from ..logging import LOGGER
+
+
+
+LOGGER("ميــوزك جاكو").info("جـارِ الاتصـال بقاعـدة البيانـات . . .")
+try:
+    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
+    mongodb = _mongo_async_.Anon
+    LOGGER("ميــوزك جاكو").info("تم الاتصـال بقاعـدة البيانـات ...✓")
+except:
+    LOGGER(__name__).error("حدث خطأ اثناء الاتصال بقاعدة البيانات.")
+    exit()
